0f07854b063c4223181701274bbffe9a42df1b0d
Sebastian Morales <sebastianmorales.js@gmail.com>
2023-02-20 21:58:21 -0500

    restored original file

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -17,23 +16,3 @@
 hello <- function() {
   print("Hello, world!")
 }
-=======
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
-}
->>>>>>> 3b2d3ad3012304ef6314e123baf4716bb01a4915
