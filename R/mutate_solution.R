d2c9abc264db90d42725548578b1689873390250
Sebastian Morales <sebastianmorales.js@gmail.com>
2023-02-26 23:02:39 -0500

    Added new scripts
    filter_solution and retrieve_answer are added and hello deprecated

---
+++ R/mutate_solution.R
@@ -0,0 +1,38 @@
+
+#' @title  mutate_solution
+#' mutate_solution() gives the solution for 5.5.2 items 1 and 2
+#' @method time_converter_mins
+#' converts time into minutes considering a 24h mins range
+#' @example
+#' mutate_solution()
+#' prints the solution in data frames
+
+library(tidyverse)
+library(nycflights13)
+
+time_converter_mins <- function(x) {
+  (x %/% 100 * 60 + x %% 100) %% 1440
+}
+
+
+mutate_solution<- function (){
+  # item1
+  flights_changes_time <- mutate(flights,
+                            dep_time_mins = time_converter_mins(dep_time),
+                            sched_dep_time_mins = time_converter_mins(sched_dep_time)
+  ) %>% select(
+    flights_times, dep_time, dep_time_mins, sched_dep_time,
+    sched_dep_time_mins
+  )
+  print("Item 1")
+  print(flight_changes_time)
+
+ # item2
+  flights_mutation <- mutate(flights,
+                             dep_time = time_converter_mins(dep_time) ,
+                             arr_time = time_converter_mins(arr_time) ,
+                             air_time_diff = air_time - arr_time + dep_time)
+  solution <-  select(flights_mutation, starts_with('dep_time'), starts_with('air_time'), starts_with('air_time_diff'))
+  print("Item 2")
+  print(solution)
+}
---
+++ R/retrieve_answer.R
@@ -0,0 +1,29 @@
+#' @title retrieve_answer
+#' retrieve_answer receive a parameter to return a solution from chapters 5
+#' of R for Data Science book
+#' @param option A integer number
+#' @examples
+#' retrieve_answer(1)
+#' retrieve_answer(2)
+#' retrieve_answer(3)
+#' retrieve_answer(4)
+#' retrieve_answer(5)
+#' retrieve_answer(6)
+#' retrieve_answer(7)
+
+library(classworkPackage)
+retrieve_answer <- function(option) {
+  switch (option,
+    1 = prime_numbers(),
+    2 = filter_solution(),
+    3 = action,
+    5 = action,
+    6 = action,
+    7 = action,
+    {
+      print("Incorrect value, instead use a integer number
+            between 1 - 7, to get a solution")
+    }
+  )
+}
+
